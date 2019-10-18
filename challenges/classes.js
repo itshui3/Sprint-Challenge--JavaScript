849a790c958f7ab6555dd20bb9646eb4691940b3
Hui <jeffrey.x.hui@gmail.com>
2019-10-18 08:33:45 -0700

    refactored pseudoclassical prototypical inheritance into class

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,17 +1,33 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-function CuboidMaker(dimensions) {
-  this.length = dimensions.length;
-  this.width = dimensions.width;
-  this.height = dimensions.height;
+// function CuboidMaker(dimensions) {
+//   this.length = dimensions.length;
+//   this.width = dimensions.width;
+//   this.height = dimensions.height;
+// }
+
+// CuboidMaker.prototype.volume = function() {
+  //   return `${this.length * this.width * this.height} is the Cuboid volume`;
+  // }
+  // CuboidMaker.prototype.surfaceArea = function() {
+  //   return `${2 * (this.length * this.width + this.length * this.height + this.width * this.height)} is the Cuboid surface area`;
+//}
+
+class CuboidMaker {
+  constructor(dimensions) {
+    this.length = dimensions.length;
+    this.width = dimensions.width;
+    this.height = dimensions.height;
+  }
+  volume() {
+    return `${this.length * this.width * this.height} is the Cuboid volume`;
+  }
+  surfaceArea() {
+    return `${2 * (this.length * this.width + this.length * this.height + this.width * this.height)} is the Cuboid surface area`;
+  }
 }
 
-CuboidMaker.prototype.volume = function() {
-  return `${this.length * this.width * this.height} is the Cuboid volume`;
-}
-CuboidMaker.prototype.surfaceArea = function() {
-  return `${2 * (this.length * this.width + this.length * this.height + this.width * this.height)} is the Cuboid surface area`;
-}
+class CubeMaker
 
 const cuboid = new CuboidMaker({
   length: 4,
@@ -20,7 +36,7 @@ const cuboid = new CuboidMaker({
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
